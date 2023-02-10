82f6f11a3589721932f23db7ea701cf02cedf328
Don <Xusi101066@163.com>
2023-02-10 16:40:15 -0500

    remove unnecessary elif

--- Lab2/unit_test/cases/case1.py
+++ Lab2/unit_test/cases/case1.py
@@ -9,9 +9,9 @@ def fizzbuzz(number):
 
     if number % 15 == 0:
         return "FizzBuzz"
-    elif number % 3 == 0:
+    if number % 3 == 0:
         return "Fizz"
-    elif number % 5 == 0:
+    if number % 5 == 0:
         return "Buzz"
 
     return str(number)
